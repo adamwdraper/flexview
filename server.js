a578cae7456374597fc3aedcae5f96c3f5b05f19
Adam Draper <adamwdraper@gmail.com>
2016-08-03 18:25:22 -0700

    update to sendFile

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
@@ -8,7 +9,7 @@ app.set('view engine', 'html');
 
 // Routes ---------------------------------------
 app.get('/\[0-9]\?', function (req, res) {
-    res.sendfile('index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Listen ---------------------------------------
