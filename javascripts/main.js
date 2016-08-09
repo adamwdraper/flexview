043e3c6fbd5dcf0bcab0187aec4678477d9acf6d
Adam Draper <adamwdraper@gmail.com>
2016-08-09 15:02:13 -0700

    horizontal dragging

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,8 +7,72 @@ $.fn.extend({
 
       $this.addClass(classes);
     });
-  },
+  }
 });
+
+
+
+$.fn.drags = function(options) {
+  const direction = this.data('handle');
+  const dragHandle = function(e) {
+    const $el = $(this);
+    const zIndex = $el.css('z-index');
+    const height = $el.outerHeight();
+    const width = $el.outerWidth();
+    const y = $el.offset().top + height - e.pageY;
+    const x = $el.offset().left + width - e.pageX;
+
+    $el.addClass('dragging');
+    
+    priorCursor = $('body').css('cursor');
+
+    $('body').css('cursor', options.cursor);
+
+    $el.css('z-index', 1000).parents().on('mousemove', function(e) {
+      const prev = $el.prev();
+      const next = $el.next(); 
+      const total = prev.outerWidth() + next.outerWidth();
+      
+      // Assume 50/50 split between prev and next then adjust to
+      // the next X for prev
+                    
+      console.log('l: ' +  prev.outerWidth() + ', r:' + next.outerWidth());
+      
+      var leftPercentage = (((e.pageX - prev.offset().left) + (x - width / 2)) / total); 
+      var rightPercentage = 1 - leftPercentage; 
+      
+      if(leftPercentage * 100 < options.min || rightPercentage * 100 < options.min) {
+        return; 
+      }
+      
+      console.log('l: ' + leftPercentage + ', r:' + rightPercentage);
+      
+      prev.css('flex', leftPercentage.toString());
+      next.css('flex', rightPercentage.toString());
+    });
+
+    $(document).on('mouseup', function() {
+      console.log('dropped');
+
+      $('body').css('cursor', priorCursor);
+      $el.removeClass('dragging').css('z-index', zIndex);
+
+      $el.parents().off('mousemove');
+    });
+
+    e.preventDefault();
+  };
+  let priorCursor;
+
+  options = $.extend({
+    cursor: direction === 'horizontal' ? 'ew-resize' : 'ns-resize',
+    min: 10
+  }, options);
+
+  return this.css('cursor', options.cursor).on('mousedown', dragHandle);
+};
+
+
 
 $(function() {
   const router = {
@@ -27,13 +91,11 @@ $(function() {
       this.activate(route);
     },
     activate: function(view) {
-      $(`[data-view="${view}"]`).activate();
+      $(`[data-view='${view}']`).activate();
     }
   };
 
-  window.onpopstate = function(event) {
-    router.activate(event.state.view);
-  };
+  $('[data-handle]').drags();
 
   $('[data-route]').on('click', function(event) {
     router.navigate($(event.currentTarget).data('route'));
