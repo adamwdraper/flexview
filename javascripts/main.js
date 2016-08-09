a16179365f345ebbce91efcf2d00279b56cbf892
Adam Draper <adamwdraper@gmail.com>
2016-08-09 15:32:26 -0700

    add function to query

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,75 +7,69 @@ $.fn.extend({
 
       $this.addClass(classes);
     });
+  },
+  drags: function(options) {
+    return this.each(function(options) {
+      const $el = $(this);
+      const direction = $el.data('handle');
+      const drag = function(e) {
+        const zIndex = $el.css('z-index');
+        const height = $el.outerHeight();
+        const width = $el.outerWidth();
+        const y = $el.offset().top + height - e.pageY;
+        const x = $el.offset().left + width - e.pageX;
+        const resize = function(e) {
+          const prev = $el.prev();
+          const next = $el.next();
+          const total = direction === 'horizontal' ? prev.outerWidth() + next.outerWidth() : prev.outerHeight() + next.outerHeight();
+          let prevPercentage;
+          let nextPercentage;
+
+          if (direction === 'horizontal') {
+            prevPercentage = (((e.pageX - prev.offset().left) + (x - width / 2)) / total);
+          } else {
+            prevPercentage = (((e.pageY - prev.offset().top) + (y - height / 2)) / total);
+          }
+          
+          nextPercentage = 1 - prevPercentage; 
+          
+          if (prevPercentage * 100 < options.min || nextPercentage * 100 < options.min) {
+            return; 
+          }
+          
+          prev.css('flex', prevPercentage.toString());
+          next.css('flex', nextPercentage.toString());
+        };
+        const drop = function() {
+          $('body').css('cursor', priorCursor);
+          $el.removeClass('dragging').css('z-index', zIndex);
+
+          $el.parents().off('mousemove');
+        };
+
+        $el.addClass('dragging').css('z-index', 1000).parents().on('mousemove', resize);
+        
+        priorCursor = $('body').css('cursor');
+
+        $('body').css('cursor', options.cursor);
+
+        $(document).on('mouseup', drop);
+
+        e.preventDefault();
+      };
+      let priorCursor;
+
+      options = $.extend({
+        cursor: direction === 'horizontal' ? 'ew-resize' : 'ns-resize',
+        min: 20
+      }, options);
+
+      console.log(direction, direction === 'horizontal', options.cursor);
+
+      return $el.css('cursor', options.cursor).on('mousedown', drag);
+    });
   }
 });
-
-
-
-$.fn.drags = function(options) {
-  return this.each(function(options) {
-    const $el = $(this);
-    const direction = $el.data('handle');
-    const drag = function(e) {
-      const zIndex = $el.css('z-index');
-      const height = $el.outerHeight();
-      const width = $el.outerWidth();
-      const y = $el.offset().top + height - e.pageY;
-      const x = $el.offset().left + width - e.pageX;
-      const resize = function(e) {
-        const prev = $el.prev();
-        const next = $el.next();
-        const total = direction === 'horizontal' ? prev.outerWidth() + next.outerWidth() : prev.outerHeight() + next.outerHeight();
-        let prevPercentage;
-        let nextPercentage;
-
-        if (direction === 'horizontal') {
-          prevPercentage = (((e.pageX - prev.offset().left) + (x - width / 2)) / total);
-        } else {
-          prevPercentage = (((e.pageY - prev.offset().top) + (y - height / 2)) / total);
-        }
-        
-        nextPercentage = 1 - prevPercentage; 
-        
-        if (prevPercentage * 100 < options.min || nextPercentage * 100 < options.min) {
-          return; 
-        }
-        
-        console.log('p: ' + prevPercentage + ', n:' + nextPercentage);
-        
-        prev.css('flex', prevPercentage.toString());
-        next.css('flex', nextPercentage.toString());
-      };
-      const drop = function() {
-        $('body').css('cursor', priorCursor);
-        $el.removeClass('dragging').css('z-index', zIndex);
-
-        $el.parents().off('mousemove');
-      };
-
-      $el.addClass('dragging').css('z-index', 1000).parents().on('mousemove', resize);
-      
-      priorCursor = $('body').css('cursor');
-
-      $('body').css('cursor', options.cursor);
-
-      $(document).on('mouseup', drop);
-
-      e.preventDefault();
-    };
-    let priorCursor;
-
-    options = $.extend({
-      cursor: direction === 'horizontal' ? 'ew-resize' : 'ns-resize',
-      min: 10
-    }, options);
-
-    console.log(direction, direction === 'horizontal', options.cursor);
-
-    return $el.css('cursor', options.cursor).on('mousedown', drag);
-  });
-};
-
 
 
 $(function() {
